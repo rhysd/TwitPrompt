443785e10436351af5fdab8962ebea237ef32187
rhysd <lin90162@yahoo.co.jp>
2012-06-10 05:02:33 +0900

    make skeltal structure and command frontend using thor

--- src/twit_prompt.rb
+++ src/twit_prompt.rb
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # ファイルの更新時間を見ればどこまでツイートを取れば良いか分かるので，ファイルに記録する必要はない
+TwitPromptConfigDir = File.expand_path('~')+'.twit_prompt'
+TwitPromptCredentialFile = TwitPromptConfigDir+"/credential.yml"
+TwitPromptTimelineData = TwitPromptConfigDir+"/timeline"
+TwitPromptCredentialTmp = '.twit_prompt_config.yml'
 
 def config_twitter
 
@@ -9,7 +13,7 @@ def config_twitter
     require 'twitter'
     require 'yaml'
 
-    yaml = YAML.load(File.open('.twit_prompt_config.yml').read)
+    yaml = YAML.load(File.open(TwitPromptCredentialTmp).read)
     Twitter.configure do |config|
         config.consumer_key = yaml['consumer_key']
         config.consumer_secret = yaml['consumer_secret']
@@ -19,14 +23,63 @@ def config_twitter
 
 end
 
+
+
+require 'thor'
+
+class TwitPrompt < Thor
+
+    desc 'init', 'initialize timeline data'
+    def init
+
+    end
+
+    desc 'put', 'get a tweet from data'
+    def one_tweet
+
+    end
+
+    desc 'update', 'update timeline'
+    def update
+
+    end
+
+    desc 'list', 'display timeline to stdout'
+    def list
+
+    end
+
+    desc 'tweet [TEXT]', 'tweet'
+    method_option :verbose, :aliases => '-v', :default => false, :desc => 'output result'
+    def tweet(text)
+        puts text
+    end
+
+    desc 'reply [TEXT]', 'reply to last-displayed tweet'
+    method_option :verbose, :aliases => '-v', :default => false, :desc => 'output result'
+    def reply(text)
+
+    end
+
+    desc 'retweet', 'retweet last-displayed tweet'
+    method_option :verbose, :aliases => '-v', :default => false, :desc => 'output result'
+    def retweet
+
+    end
+
+    desc 'fav', 'add last-desplayed tweet to favorite tweets'
+    method_option :verbose, :aliases => '-v', :default => false, :desc => 'output result'
+    def fav
+
+    end
+
+end
+
 #
 # main
 #
 if __FILE__ == $0 then
-
-    config_twitter
-
-
-
+    TwitPrompt.start
 end
+
 
