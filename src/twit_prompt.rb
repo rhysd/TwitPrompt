8cd96a5848ddd930bf3b4e2c10242181a556350a
rhysd <lin90162@yahoo.co.jp>
2012-10-02 19:00:07 +0900

    Change whole structure for maintainablity and scalability
    - add test file 'hoge.rb' to .gitignore
    - export app's configuration to TwitPromptConfig
    - export Twitter configuration to TwitPromptConfig
    - export timeline maintainanse to Timeline
    - delete update subcommand because of duplicate feature

--- src/twit_prompt.rb
+++ src/twit_prompt.rb
@@ -1,249 +1,264 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
+# encoding: utf-8
 
-# ファイルの更新時間を見ればどこまでツイートを取れば良いか分かるので，ファイルに記録する必要はない
-# execute in background and quietly
-Process.daemon true,true if %w[init update].any?{|a| a =~ /^#{ARGV[0]}/}
+# TODO def_feature -> def_command
+
+# Extend String for making colorful {{{
 
 # methods to colorize
 class String
 
-    def self.methods_to_colorize(color_codes)
-        color_codes.each do |color,code|
-            define_method(color) do
-                "\e[#{code}m#{self}\e[0m"
-            end
-        end
+  def self.methods_to_colorize(color_codes)
+    color_codes.each do |color,code|
+      define_method(color) do
+        "\e[#{code}m#{self}\e[0m"
+      end
     end
+  end
 
-    # private :colorize_method
+  # private :colorize_method
 
-    methods_to_colorize({
-        dark_blue:   "0;34",
-        dark_green:  "0;32",
-        dark_cyan:   "0;36",
-        dark_red:    "0;31",
-        dark_purple: "0;35",
-        dark_yellow: "0;33",
-        red:         "1;31",
-        blue:        "1;34",
-        en:          "1;32",
-        cyan:        "1;36",
-        red:         "1;31",
-        purple:      "1;35",
-        yellow:      "1;33",
-        black:       "0;30",
-        dark_gray:   "0;37",
-        gray:        "1;30",
-        white:       "1;37"
-    })
+  methods_to_colorize({
+    dark_blue:   "0;34",
+    dark_green:  "0;32",
+    dark_cyan:   "0;36",
+    dark_red:    "0;31",
+    dark_purple: "0;35",
+    dark_yellow: "0;33",
+    red:         "1;31",
+    blue:        "1;34",
+    en:          "1;32",
+    cyan:        "1;36",
+    red:         "1;31",
+    purple:      "1;35",
+    yellow:      "1;33",
+    black:       "0;30",
+    dark_gray:   "0;37",
+    gray:        "1;30",
+    white:       "1;37"
+  })
 
 end
+# }}}
 
-module TwitPrompt extend self
+module TwitPromptConfig extend self
+  Root = File.expand_path('~')+'/.twit_prompt'
+  # File = TwitPromptConfigDir+"/credential.yml"
+  Setting = '.twit_prompt_config.yml'
+  Cache = "/tmp/timeline"
+  ScreenName = "@Linda_pp"
 
-    TwitPromptConfigDir = File.expand_path('~')+'/.twit_prompt'
-    # TwitPromptCredentialFile = TwitPromptConfigDir+"/credential.yml"
-    TwitPromptCredentialFile = '.twit_prompt_config.yml'
-    TwitPromptTimelineData = "/tmp/timeline"
-    UserName = "@Linda_pp"
-
-    def check_config
-
-        Dir.mkdir TwitPromptConfigDir unless File.exist? TwitPromptConfigDir
-        unless File.exist? TwitPromptCredentialFile
-            File.open(TwitPromptCredentialFile, "w") do |file|
-                file.print <<-EOS.gsub(/^\s+/, '')
+  def check_config
+    Dir.mkdir Root unless Dir.exist? Root
+    unless File.exist? Setting
+      File.open(Setting, "w") do |file|
+        file.print <<-EOS.gsub(/^\s+/, '')
                         # get app keys at https://dev.twitter.com/ and write them
                         consumer_key:       YourConsumerKey
                         consumer_secret:    YourConsumerSecretKey
                         oauth_token:        YourOAuthToken
                         oauth_token_secret: YourOAuthSecretToken
-                EOS
+        EOS
 
-                STDERR.puts "Configuration-keys are not found."
-                STDERR.puts "Write your consumer keys and OAuth keys to #{TwitPromptCredentialFile}"
-            end
-            system 'bash', '-c', (ENV['EDITOR'] || 'vi')+' "$@"', '--', TwitPromptCredentialFile
-            false
-        else
-            true
-        end
-
+        STDERR.puts "Configuration-keys are not found."
+        STDERR.puts "Write your consumer keys and OAuth keys to #{Setting}"
+      end
+      system 'bash', '-c', (ENV['EDITOR'] || 'vi')+' "$@"', '--', Setting
     end
+  end
 
-    def config_twitter
-        return if @already_authorized
+  def config_twitter
+    require 'twitter'
+    require 'yaml'
 
-        require 'twitter'
-        require 'yaml'
+    check_config
 
-        check_config
+    yaml = YAML.load(File.open(Setting).read)
+    Twitter::client.new(
+      consumer_key: yaml['consumer_key'],
+      consumer_secret: yaml['consumer_secret'],
+      oauth_token: yaml['oauth_token'],
+      oauth_token_secret: yaml['oauth_token_secret']
+    )
+  end
 
-        yaml = YAML.load(File.open(TwitPromptCredentialFile).read)
-        Twitter.configure do |config|
-            config.consumer_key = yaml['consumer_key']
-            config.consumer_secret = yaml['consumer_secret']
-            config.oauth_token = yaml['oauth_token']
-            config.oauth_token_secret = yaml['oauth_token_secret']
-        end
+  private :check_config, :config_twitter
 
-        @already_authorized = true
-    end
-
-    def filtering? status
-        false
-    end
-
-    def update_timeline
-        config_twitter
-        last_update = File.exist?(TwitPromptTimelineData) ?
-            File.atime(TwitPromptTimelineData) :
-            Time.local(1900)
-        File.open TwitPromptTimelineData,"w+" do |file|
-            Twitter.home_timeline.reverse_each do |status|
-                break if status.created_at < last_update
-                unless filtering? status
-                    user = status.user.screen_name
-                    text = status.text.gsub /\n/,' '
-                    created_at = status.created_at
-                    file.puts created_at,user,text
-                end
-            end
-        end
-    end
-
-    def reply?(text)
-        text =~ /^#{UserName}/
-    end
-
-    def mention?(text)
-        text.include? UserName
-    end
-
-    def rt?(text)
-        text =~ /^RT @[a-zA-Z0-9_]+: /
-    end
-
-    def build_tweet(user,text,created_at)
-        user = "@#{user}: ".dark_cyan
-        text = text.gsub /\n/,' '
-        text = text.include?(UserName) ? text.dark_green : text # whether mention or not
-        created_at = ' [' + Time.new(created_at).strftime("%m/%d %T") + ']'
-        created_at = created_at.dark_yellow
-        user + text + created_at
-    end
-
-    def stored_timeline
-        File.open TwitPromptTimelineData,"r" do |file|
-
-        end
-    end
-
-    private :check_config,
-            :config_twitter,
-            :filtering?,
-            :update_timeline,
-            :build_tweet,
-            :reply?,
-            :mention?,
-            :rt?
-
-    def init(options)
-        update_timeline
-    end
-
-    def put(options)
-        puts build_tweet("Linda_pp","aiueo @Linda_pp kakikukeko sasissuseso","2012-06-30 11:35:36 +0900")
-    end
-
-    def update(options)
-        update_timeline
-    end
-
-    def listup(options)
-
-    end
-
-    def tweet(options,text)
-        puts text
-        puts "tweeted: #{text}" if options[:verbose]
-    end
-
-    def reply(options,text)
-        puts "replied: #{text}" if options[:verbose]
-    end
-
-    def retweet(options)
-        puts "retweeted: " if options[:verbose]
-    end
-
-    def fav(options)
-        puts "faved: " if options[:verbose]
-    end
-
-    def config(options)
-        if check_config
-            puts "open #{TwitPromptCredentialFile}"
-            system 'bash', '-c', (ENV['EDITOR'] || 'vi')+' "$@"', '--', TwitPromptCredentialFile
-        end
-    end
+  def client
+    @client ||= config_twitter
+  end
 
 end
 
+# maintain timeline cache
+class Timeline
+
+  def head
+    # head of cached tweets
+    #  not implemented
+  end
+
+  def all
+    # list of cached tweets
+    # not implemented
+  end
+
+  def construct
+    File.delete(TwitPromptConfig::Setting)
+    update
+  end
+
+  def update
+    Process.daemon true,true
+    # not implemented
+    # get timelines from the file
+    twitter = TwitPromptConfig::client
+    last_update = File.exist?(@cache) ?
+      File.atime(TwitPromptConfig::Cache) :
+      Time.local(1900)
+    File.open(TwitPromptConfig::Cache,"w+") do |file|
+      twitter.home_timeline.reverse_each do |status|
+        break if status.created_at < last_update
+        unless filtering? status
+          user = status.user.screen_name
+          text = status.text.gsub /\n/,' '
+          created_at = status.created_at
+          file.puts created_at,user,text
+        end
+      end
+    end
+  end
+
+  private :update
+
+end
+
+# Main logic of TwitPrompt {{{
+module TwitPrompt extend self
+
+  @timeline = Timeline.new
+  @screen_name = TwitPromptConfig::ScreenName
+
+  def filtering? status
+    false
+  end
+
+  def reply?(text)
+    text =~ /^#{@screen_name}/
+  end
+
+  def mention?(text)
+    text.include? @screen_name
+  end
+
+  def rt?(text)
+    text =~ /^RT @[a-zA-Z0-9_]+: /
+  end
+
+  def build_tweet(user,text,created_at)
+    user = "@#{user}: ".dark_cyan
+    text = text.gsub /\n/,' '
+    text = text.include?(@screen_name) ? text.dark_green : text # whether mention or not
+    created_at = ' [' + Time.new(created_at).strftime("%m/%d %T") + ']'
+    created_at = created_at.dark_yellow
+    user + text + created_at
+  end
+
+  private :filtering?,
+          :build_tweet,
+          :reply?,
+          :mention?,
+          :rt?
+
+  def init(options)
+    @timeline.construct
+  end
+
+  def put(options)
+    puts build_tweet("Linda_pp","aiueo @Linda_pp kakikukeko sasissuseso","2012-06-30 11:35:36 +0900")
+  end
+
+  def listup(options)
+
+  end
+
+  def tweet(options,text)
+    puts text
+    puts "tweeted: #{text}" if options[:verbose]
+  end
+
+  def reply(options,text)
+    puts "replied: #{text}" if options[:verbose]
+  end
+
+  def retweet(options)
+    puts "retweeted: " if options[:verbose]
+  end
+
+  def fav(options)
+    puts "faved: " if options[:verbose]
+  end
+
+  def config(options)
+    system 'bash', '-c', (ENV['EDITOR'] || 'vi')+' "$@"', '--', TwitPromptConfig::Setting
+  end
+
+end
+# }}}
+
+# Interface of TwitPrompt # {{{
 require 'thor'
 class TwitPromptApp < Thor
 
-    private
+  private
 
-    def self.def_feature(name)
-        define_method name do |*args|
-            TwitPrompt::__send__ name,options,*args
-        end
+  def self.def_feature(name)
+    define_method name do |*args|
+      TwitPrompt::__send__ name,options,*args
     end
+  end
 
-    def self.verbose_option
-        method_option :verbose, :type => :boolean, :aliases => '-v', :default => true, :desc => 'output result'
-    end
+  def self.verbose_option
+    method_option :verbose, :type => :boolean, :aliases => '-v', :default => true, :desc => 'output result'
+  end
 
-    public
+  public
 
-    desc 'init', 'initialize timeline data'
-    def_feature :init
+  desc 'init', 'initialize timeline cache'
+  def_feature :init
 
-    desc 'put', 'get a tweet from data'
-    def_feature :put
+  desc 'put', 'show a tweet'
+  def_feature :put
 
-    desc 'update', 'update timeline'
-    def_feature :update
+  desc 'listup', 'show timeline to stdout'
+  def_feature :listup
 
-    desc 'listup', 'display timeline to stdout'
-    def_feature :listup
+  desc 'tweet [TEXT]', 'tweet TEXT'
+  def_feature :tweet
 
-    desc 'tweet [TEXT]', 'tweet'
-    def_feature :tweet
+  desc 'reply [TEXT]', 'reply to last-displayed tweet'
+  verbose_option
+  def_feature :reply
 
-    desc 'reply [TEXT]', 'reply to last-displayed tweet'
-    verbose_option
-    def_feature :reply
+  desc 'retweet', 'retweet last-displayed tweet'
+  verbose_option
+  def_feature :retweet
 
-    desc 'retweet', 'retweet last-displayed tweet'
-    verbose_option
-    def_feature :retweet
+  desc 'fav', 'add last-desplayed tweet to favorite tweets'
+  verbose_option
+  def_feature :fav
 
-    desc 'fav', 'add last-desplayed tweet to favorite tweets'
-    verbose_option
-    def_feature :fav
-
-    desc 'config', 'configure YAML setting file'
-    def_feature :config
+  desc 'config', 'configure YAML setting file'
+  def_feature :config
 end
+# }}}
 
 #
 # main
 #
 if __FILE__ == $0 then
-    TwitPromptApp::start
+  TwitPromptApp::start
 end
 
 
